237f275af3f6230dcb0004c0c09375ed4dafce12
trashhcatproductionsltd <51205052+TheGareBear@users.noreply.github.com>
2023-12-07 16:02:56 -0700

    Racer position script added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,38 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public List<CarController> cars = new List<CarController>();
 
+    public float postiionUpdateRate = 0.05f;
+    private float lastPositionUpdateTime;
+
     void Awake() {
         instance = this;
+    }
+
+    void Update()
+    {
+        if(Time.time - lastPositionUpdateTime > postiionUpdateRate) {
+            lastPositionUpdateTime = Time.time;
+            UpdateCarRacePositions();
+        }
+    }
+
+    void UpdateCarRacePositions() {
+        cars.Sort(SortPosition);
+
+        for(int x = 0; x < cars.Count; x ++) {
+            cars[x].racePosition = cars.Count - x;
+        }
+    }
+
+    int SortPosition(CarController a, CarController b) {
+        if(a.zonesPassed > b.zonesPassed) 
+            return 1;
+        else if(b.zonesPassed > a.zonesPassed)
+            return -1;
+        
+        float aDist = Vector3.Distance(a.transform.position, a.curTrackZone.transform.position);
+        float bDist = Vector3.Distance(b.transform.position, b.curTrackZone.transform.position);
+
+        return aDist > bDist ? 1 : -1;
     }
 }
